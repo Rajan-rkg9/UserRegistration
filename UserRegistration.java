6b76f9d3516cd1a70d970455d128ea5f095c8bf8
Rajan-rkg9 <amirajangupta@gmail.com>
2020-10-01 20:41:44 +0530

    [Rajan-rkg9]Add. Valid Password including Rule1 added

--- UserRegistration.java
+++ UserRegistration.java
@@ -8,16 +8,16 @@ public class UserRegistration {
 		boolean flag = true;
 		while(flag)
 		{
-			System.out.println("Enter A Valid Mobile Number : ");
-			String phoneNumber = sc.nextLine();
-			String pattern = "(91) [0-9]{10}";
-			if(phoneNumber.matches(pattern)) 
+			System.out.println("Enter A Valid Password : ");
+			String password = sc.nextLine();
+			String pattern = ".{8,}";
+			if(password.matches(pattern)) 
 			{
-				System.out.println("Valid Phone Number");
+				System.out.println("Valid Password");
 				flag = false;
 			}
 			else
-				System.out.println("Invalid Phone Number");
+				System.out.println("Invalid Password");
 		}
 	}
 }
