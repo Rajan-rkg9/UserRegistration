a84e4530794ae696386b5e6c50287771efdd0633
Rajan-rkg9 <amirajangupta@gmail.com>
2020-10-01 18:04:52 +0530

    [Rajan-rkg9]Add. Valid first name added

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,5 +1,23 @@
 package com.Capgemini.Registration;
-
+import java.util.*;
+import java.io.*;
 public class UserRegistration {
-
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		boolean flag = true;
+		while(flag)
+		{
+			System.out.println("Enter A Valid First Name: ");
+			String firstName = sc.nextLine();
+			String pattern = "Cap[a-zA-Z\\s]{3,}";
+			if(firstName.matches(pattern)) 
+			{
+				System.out.println("Valid First Name");
+				flag = false;
+			}
+			else
+				System.out.println("Invalid First Name");
+		}
+	}
 }
