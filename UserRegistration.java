3de2bb4b11d00ce75a1ead308d049e391d974ee2
Rajan-rkg9 <amirajangupta@gmail.com>
2020-10-03 09:39:45 +0530

    [Rajan-rkg9]Add. Valid last name added

--- UserRegistration.java
+++ UserRegistration.java
@@ -10,7 +10,7 @@ public class UserRegistration {
 		{
 			System.out.println("Enter A Valid Last Name: ");
 			String lastName = sc.nextLine();
-			String pattern = "Cap[a-zA-Z\\s]{3,}";
+			String pattern = "[A-Z]{1}[a-zA-Z\\s]{2,}";
 			if(lastName.matches(pattern)) 
 			{
 				System.out.println("Valid Last Name");
